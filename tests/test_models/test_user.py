348e117d2b697555b0e0975c332ab2504caf5754
abeleyasu <abeleyasu98@gmail.com>
2024-07-19 03:11:52 -0700

    airbnb clone v3

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,84 +12,107 @@ User = models.user.User
 BaseModel = models.base_model.BaseModel
 storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
+
 class TestUserDocs(unittest.TestCase):
-    """Class for testing documentation of User Class"""
+    """Class for testing User Class docs"""
 
     @classmethod
     def setUpClass(cls):
-        """Set up for documentation tests"""
         print('\n\n.................................')
         print('..... Testing Documentation .....')
         print('........   User  Class   ........')
         print('.................................\n\n')
 
     def test_doc_file(self):
-        """Test documentation for the file"""
+        """... documentation for the file"""
         expected = '\nUser Class from Models Module\n'
         actual = models.user.__doc__
         self.assertEqual(expected, actual)
 
     def test_doc_class(self):
-        """Test documentation for the User class"""
+        """... documentation for the class"""
         expected = 'User class handles all application users'
         actual = User.__doc__
         self.assertEqual(expected, actual)
 
+
 class TestUserInstances(unittest.TestCase):
-    """Testing User class instances"""
+    """testing for class instances"""
 
     @classmethod
     def setUpClass(cls):
-        """Set up for instance tests"""
         print('\n\n.................................')
         print('....... Testing Functions .......')
         print('.........  User  Class  .........')
         print('.................................\n\n')
 
     def setUp(self):
-        """Initialize a new User instance for testing"""
+        """initializes new user for testing"""
         self.user = User()
 
     def test_instantiation(self):
-        """Test if User is properly instantiated"""
+        """... checks if User is properly instantiated"""
         self.assertIsInstance(self.user, User)
 
-    @unittest.skipIf(storage_type == 'db', 'skip if environment is db')
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
     def test_to_string(self):
-        """Test if User is properly casted to string"""
+        """... checks if BaseModel is properly casted to string"""
         my_str = str(self.user)
         my_list = ['User', 'id', 'created_at']
-        actual = sum(sub_str in my_str for sub_str in my_list)
-        self.assertTrue(actual == 3)
+        actual = 0
+        for sub_str in my_list:
+            if sub_str in my_str:
+                actual += 1
+        self.assertTrue(3 == actual)
 
-    @unittest.skipIf(storage_type == 'db', 'skip if environment is db')
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
     def test_instantiation_no_updated(self):
-        """Test User instance should not have 'updated_at' attribute initially"""
+        """... should not have updated attribute"""
+        self.user = User()
         my_str = str(self.user)
-        self.assertNotIn('updated_at', my_str)
+        actual = 0
+        if 'updated_at' in my_str:
+            actual += 1
+        self.assertTrue(0 == actual)
 
-    @unittest.skipIf(storage_type == 'db', 'skip if environment is db')
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
     def test_updated_at(self):
-        """Test save function should update 'updated_at' attribute"""
-        old_updated_at = self.user.updated_at
+        """... save function should add updated_at attribute"""
         self.user.save()
-        self.assertNotEqual(old_updated_at, self.user.updated_at)
+        actual = type(self.user.updated_at)
+        expected = type(datetime.now())
+        self.assertEqual(expected, actual)
 
-    @unittest.skipIf(storage_type == 'db', 'skip if environment is db')
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
     def test_to_json(self):
-        """Test to_json should return a serializable dict object"""
-        user_json = self.user.to_json()
+        """... to_json should return serializable dict object"""
+        self.user_json = self.user.to_json()
+        actual = 1
         try:
-            json.dumps(user_json)
-            serializable = True
-        except (TypeError, OverflowError):
-            serializable = False
-        self.assertTrue(serializable)
+            serialized = json.dumps(self.user_json)
+        except:
+            actual = 0
+        self.assertTrue(1 == actual)
 
-    @unittest.skipIf(storage_type == 'db', 'skip if environment is db')
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
     def test_json_class(self):
-        """Test to_json should include a class key with value User"""
-        user_json = self.user.to_json()
-        self.assertIn('__class__', user_json)
-        self.assertEqual(user_json['__class__'], 'User')
+        """... to_json should include class key with value User"""
+        self.user_json = self.user.to_json()
+        actual = None
+        if self.user_json['__class__']:
+            actual = self.user_json['__class__']
+        expected = 'User'
+        self.assertEqual(expected, actual)
 
+    def test_email_attribute(self):
+        """... add email attribute"""
+        self.user.email = "bettyholbertn@gmail.com"
+        if hasattr(self.user, 'email'):
+            actual = self.user.email
+        else:
+            actual = ''
+        expected = "bettyholbertn@gmail.com"
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main
